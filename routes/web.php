f667f460a321d1afb5fd56f7707e4ac98f5afecb
conejofoz <conejofoz@gmail.com>
2021-09-14 08:19:57 -0300

    models e tinker

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/queries', function(){
+    //$events = \App\Models\Event::all();
+    //return $events;
+    $event = \App\Models\Event::where('id',1)->first();
+    return $event;
+});
