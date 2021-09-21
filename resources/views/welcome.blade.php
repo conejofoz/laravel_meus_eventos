82ee61c350a752ca7cb47a1fefa78700f5eeb2f1
conejofoz <conejofoz@gmail.com>
2021-09-21 00:04:40 -0300

    template de eventos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,37 @@
-<h2>Eventos</h2>
-<hr>
+@extends('layout.site')
 
-<ul>
-    @foreach ($events as $event)
-        <li>{{$event->title}}</li>
-    @endforeach
-</ul>
+@section('title') Lista de eventos @endsection
 
-<hr>
+@section('content')
 
-<ul>
-    @forelse ($events as $event)
-        <li>{{$event->title}}</li>
+    <div class="col-12">
+        <h2>Eventos</h2>
+        <hr>
+    </div>
 
-        @empty
-            <li>Nenhum evento cadastrado</li>
-    @endforelse
-</ul>
+
+    <div class="row mb-4">
+        
+            @forelse ($events as $event)
+                <div class="col-4">
+                    <div class="card">
+                        <img src="https://via.placeholder.com/640x480.png/00ee88?text=ut" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$event->title}}</h5>
+                            <strong>Acontece em: {{$event->start_event}}</strong>
+                            <p class="card-text">{{$event->description}}</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- a cada 3 fechar uma div e abrir uma div row --}}
+                @if (($loop->iteration % 3) == 0)
+                    </div> <div class="row mb-4">
+                @endif
+            @empty
+                <div class="alert alert-warning">Nenhum evento cadastrado</div>
+            @endforelse
+    </div>
+
+
+    
+@endsection
