1cd43df25f4e6f4b137dfeddcfc88a001f190190
conejofoz <conejofoz@gmail.com>
2021-09-25 20:51:30 -0300

    CORREÇÃO COMMIT ANTERIOR - validação controller - mensagem de erro input a input no template

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -21,41 +21,49 @@
                 <div class="form-group">
                     <label for="">Título Evento</label>
                     <input type="text" class="form-control @if($errors->has('title')) is-invalid @endif" name="title">
-                    <div class="invalid-feedback">
-                        @foreach ($errors->get('title') as $error)
-                            {{$error}}
-                        @endforeach
-                    </div>
+                    @if ($errors->has('title'))
+                        <div class="invalid-feedback">
+                            @foreach ($errors->get('title') as $error)
+                                {{$error}}
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
 
                 <div class="form-group">
                     <label for="">Descrição Rápida Evento</label>
                     <input type="text" class="form-control @if($errors->has('description')) is-invalid @endif" name="description">
-                    <div class="invalid-feedback">
-                        @foreach ($errors->get('description') as $error)
-                            {{$error}}
-                        @endforeach
-                    </div>
+                    @if ($errors->has('description'))
+                        <div class="invalid-feedback">
+                            @foreach ($errors->get('title') as $error)
+                                {{$error}}
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
 
                 <div class="form-group">
                     <label for="">Fale mais sobre o evento</label>
                     <textarea class="form-control @if($errors->has('body')) is-invalid @endif" name="body" id="" cols="30" rows="10"></textarea>
-                    <div class="invalid-feedback">
-                        @foreach ($errors->get('title') as $error)
-                            {{$error}}
-                        @endforeach
-                    </div>
+                    @if ($errors->has('body'))
+                        <div class="invalid-feedback">
+                            @foreach ($errors->get('title') as $error)
+                                {{$error}}
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
 
                 <div class="form-group">
                     <label for="">Quando vai acontecer?</label>
                     <input type="text" class="form-control @if($errors->has('start_event')) is-invalid @endif" name="start_event">
-                    <div class="invalid-feedback">
-                        @foreach ($errors->get('start_event') as $error)
-                            {{$error}}
-                        @endforeach
-                    </div>
+                    @if ($errors->has('start_event'))
+                        <div class="invalid-feedback">
+                            @foreach ($errors->get('title') as $error)
+                                {{$error}}
+                            @endforeach
+                        </div>
+                    @endif
                 </div>
 
                 <button type="submit" class="btn btn-lg btn-success">Criar Evento</button>
