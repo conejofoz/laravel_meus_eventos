6182868ba0757b71af38bfd4c1edba9179606f93
conejofoz <conejofoz@gmail.com>
2021-11-12 23:59:03 -0300

    verificação de conta funcionando

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifique seu E-mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Reenviamos um link novo de verificação de conta.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                    {{ __('Antes de continuar, por favor verifique seu E-mail, enviamos un link de ativação.') }}
+                    {{ __('Se voce não recebeu o E-mail.') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('clique aqui para receber um novo link.') }}</button>.
                     </form>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +30,7 @@ Route::prefix('/enrollment')->name('enrollment.')->group(function(){
 });
 
 
-Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function(){
+Route::middleware('auth', 'verified')->prefix('/admin')->name('admin.')->group(function(){
    
     Route::resource('events', \App\Http\Controllers\Admin\EventController::class);
 
@@ -41,6 +45,22 @@ Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function(){
 
 
 
+
+ Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
  
 
  
